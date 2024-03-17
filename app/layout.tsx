a09d36af9ba1e486dc2ce7c1b55b24178f604b0c
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-03-17 17:01:12 +0430

    install RadixUl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
+
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme accentColor="iris">
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
