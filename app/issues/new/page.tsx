622afd66ecacbede7a8f9955b85ebca02ee481ac
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-03-17 21:26:44 +0430

    build markdownEditor.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,20 @@
 "use client";
-import { Box, Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Box, Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useEffect, useRef } from "react";
 
 const NewIssuePage = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    ref.current?.focus();
+  });
   return (
     <Box className="max-w-xl space-y-4">
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" ref={ref} />
       </TextField.Root>
-      <TextArea placeholder="Description..." />
+      <SimpleMDE placeholder="Description..." />
       <Button>Submit New Issue</Button>
     </Box>
   );
