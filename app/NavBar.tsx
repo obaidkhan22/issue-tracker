21245e1be56c5a4dd021f6043264e6f16c397fdc
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-03-17 16:06:13 +0430

    highlight the active link.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { FaBug } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const currentPath = usePathname();
   return (
     <nav className="flex space-x-6 px-6 h-16 mb-5 items-center border-b">
       <Link href="/">
@@ -14,7 +17,9 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
-            className="text-zinc-400 hover:text-zinc-900 transition-colors duration-500"
+            className={` ${
+              currentPath === link.href ? "text-zinc-900" : "text-zinc-400"
+            } hover:text-zinc-900 transition-colors duration-500`}
             key={link.href}
             href={link.href}
           >
