f14cf881b4ce580734413610e2198e66e58f547a
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-03-28 02:18:20 +0430

    use the cache function in react for getting issue with specific id.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -8,14 +8,18 @@ import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 import AssigneeSelector from "./AssigneeSelector";
 import IssueStatusSelector from "./IssueStatusSelector";
+import { cache } from "react";
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId },
+  })
+);
 interface Props {
   params: { id: string };
 }
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   if (!issue) notFound();
   return (
     <Grid columns={{ initial: "1", sm: "5" }} gap="5">
@@ -39,9 +43,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 export default IssueDetailPage;
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   return {
     title: issue?.title,
     description: `Details related to issue with id = ${issue?.id}`,
