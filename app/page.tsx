a35dd3cf61839258ba5b8ca2ff00835935d0584c
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-03-27 05:03:26 +0430

    build layout for dashboard section. and add the required components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+import { Flex, Grid } from "@radix-ui/themes";
 import IssueChart from "./IssueChart";
 import LatestIssues from "./LatestIssues";
+import IssuesSummary from "./IssuesSummary";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-  return <IssueChart open={20} inProgress={10} close={15} />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const close = await prisma.issue.count({ where: { status: "CLOSE" } });
+  const props = { open, inProgress, close };
+  return (
+    <>
+      <Grid columns={{ initial: "1", md: "2" }} gap="5">
+        <Flex direction="column" gap="5">
+          <IssuesSummary {...props} />
+          <IssueChart {...props} />
+        </Flex>
+        <LatestIssues />
+      </Grid>
+    </>
+  );
 }
